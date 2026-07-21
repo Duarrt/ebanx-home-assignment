3eae8fa92463c4513f38fd0f4549cf4b4a75cf62
Ramon Ribeiro <ramonduaribeiro@gmail.com>
2026-07-21 19:52:52 -0300

    Fix: improve EventType to accept case-insensitive types

--- src/main/java/br/com/eha/enums/EventType.java
+++ src/main/java/br/com/eha/enums/EventType.java
@@ -1,7 +1,14 @@
 package br.com.eha.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum EventType {
     DEPOSIT,
     WITHDRAW,
-    TRANSFER
+    TRANSFER;
+
+    @JsonCreator
+    public static EventType fromValue(String value) {
+        return EventType.valueOf(value.toUpperCase());
+    }
 }
