fc5c9e1c9ed9994b2592db7694ba2f5ce561e5d6
Ramon Ribeiro <ramonduaribeiro@gmail.com>
2026-07-21 19:12:38 -0300

    WIP: Creating Project Structure

---
+++ src/main/java/br/com/eha/ebanxHomeAssignmentApplication.java
@@ -0,0 +1,13 @@
+package br.com.eha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ebanxHomeAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ebanxHomeAssignmentApplication.class, args);
+	}
+
+}
